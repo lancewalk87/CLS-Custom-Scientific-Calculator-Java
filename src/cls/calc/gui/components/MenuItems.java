d465c17f8e1303fe9594c02dec6dee949c395d26
Lance Walker <walkerlance65@gmail.com>
2018-11-16 08:04:35 -0700

    v1.0.5

--- src/cls/calc/gui/components/MenuItems.java
+++ src/cls/calc/gui/components/MenuItems.java
@@ -12,37 +12,90 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 
-public class MenuItems extends MenuBar {
-	/* Menu Items */ 	
-	public MenuBar menuBar() {
-		MenuBar menuBar = new MenuBar();
-		
+public class MenuItems {
+	/* MenuBar: Types */ 	
+	static MenuBar menuBar = new MenuBar(); 
+	
+	public static MenuBar debug_menubar() {		/* Developer Tools */ 
+		menuBar.getMenus().addAll(
+				fileItem(),
+				editItem(),
+				logItem(),
+				windowItem(),
+				preferencesItem(),
+				helpItem());
+		System.out.println("cls.calc.gui.componets: debug_menubar()");
 		return menuBar; 
 	}
 	
-	public static void config() {
-		Menu fileMenu = new Menu("File");		
-		fileMenu.getItems().addAll(
-	            new MenuItem("New File..."),
-	            new MenuItem("Open file..."),
+	public static MenuBar dist_menubar() {		/* Distribution Tools */ 
+		menuBar.getMenus().addAll(
+				fileItem(),
+				editItem(),
+				preferencesItem(),
+				helpItem());	
+		System.out.println("cls.calc.gui.componets: dist_menubar()");
+		return menuBar; 
+	}
+	
+	/* MenuBar: Items */ 
+	private static Menu fileItem() {			// Add/Remove Existing Equations | Start/End Session 
+		Menu m = new Menu("File");
+		m.getItems().addAll(
+	            new MenuItem("New File"),
+	            new MenuItem("Open file"),
 	            new MenuItem("Save file"));
-	    fileMenu.setId("#fileMenu");
+	    m.setId("#fileMenu"); 
+		return m; 
 	}
 	
-	/* Menu Items */ 
-	private void fileItem() {
-		
+	private static Menu editItem() { 			// Text Options
+		Menu m = new Menu("Edit");
+		m.getItems().addAll(
+	            new MenuItem("Undo"),
+	            new MenuItem("Redo"),
+	            new MenuItem("Cut"),
+	            new MenuItem("Copy"),
+	            new MenuItem("Paste"));
+	    m.setId("#editMenu"); 
+		return m; 
 	}
 	
-	private void logItem() {
-		
+	private static Menu logItem() {			// View Session Logs 
+		Menu m = new Menu("Log");
+		m.getItems().addAll(
+	            new MenuItem("Open Log"),
+	            new MenuItem("Close Log"));
+	    m.setId("#logMenu"); 
+		return m; 
 	}
 	
-	private void windowItem() {
-		
+	private static Menu windowItem() {			// Add/Remove UI Component in Editor 
+		Menu m = new Menu("Window");
+		m.getItems().addAll(
+	            new MenuItem("New Tab"),
+	            new MenuItem("Close Tab"),
+	            new MenuItem("Close Window"));
+	    m.setId("#windowMenu"); 
+		return m; 
 	}
 	
-	private void helpItem() {
-		
+	private static Menu preferencesItem() {	// User Preferences Menu 	
+		Menu m = new Menu("Preferences");
+		m.getItems().addAll(
+	            new MenuItem("Theme Options"),
+	            new MenuItem("Keyboards"),
+	            new MenuItem("File Output"));
+	    m.setId("#preferencesMenu"); 
+		return m; 
+	}
+	
+	private static Menu helpItem() {			// Application Help/Info 
+		Menu m = new Menu("Help");
+		m.getItems().addAll(
+	            new MenuItem("About CLS Calculator"),
+	            new MenuItem("Dev Connect Details")); 
+	    m.setId("#helpMenu"); 
+		return m; 
 	}
 }
--- src/cls/calc/main/Main.java
+++ src/cls/calc/main/Main.java
@@ -7,10 +7,12 @@ package cls.calc.main;
  * Copyright © 2018 CodeLife Productions. All rights reserved.
  */
 
+import java.awt.Button;
 /* Imports */
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.application.Preloader.PreloaderNotification;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -21,14 +23,17 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 // local packages
-import cls.calc.gui.components.*; 
+import cls.calc.gui.components.MenuItems; 
 import cls.calc.sys.*;	
 import cls.calc.service.*;
 
 abstract interface ApplicationDelegate {
+	/* Application ENV Variables */ 
+	static boolean DEV_MODE = true; // Controls Debug Interface
 	/* Application Events */ 
 	void applicationDidOpen();		// Application Event: open 
 	void applicationDidDealoc(); 	// Application Event: memory deallocated
@@ -36,15 +41,15 @@ abstract interface ApplicationDelegate {
 	void applicationDidClose();		// Application Event: closed 
 }
 
-public class Main extends Application implements ApplicationDelegate { // Entry 
+public class Main extends Application implements ApplicationDelegate { // Main 
 	/* Application Properties */ 
-	private Stage primaryStage;
-	private AnchorPane anchorPane;
-	MenuBar menuBar = new MenuBar();
-	
+	private Stage primaryStage;				// 
+	private AnchorPane anchorPane;			// 
+	MenuBar menuBar = new MenuBar(); 		// 
+
 	/* Functions: Application RunCycle */
 	public static void main(String args[]) { launch(args); }
-
+	
 	// JFX Application Overrides 
 	@Override public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -52,11 +57,11 @@ public class Main extends Application implements ApplicationDelegate { // Entry
 		
 		// construct interface: 
 		constructInterface(); 
-		System.out.println("calc: Start Application");
+		System.out.println("CLS Calculator: Start Application");
 	}
 	
 	@Override public void stop() {
-		System.out.println("calc: Stop Application"); 
+		System.out.println("CLS Calculator: Stop Application"); 
 	}
 	
 	 public void handleApplicationNotification(PreloaderNotification info) {
@@ -69,9 +74,17 @@ public class Main extends Application implements ApplicationDelegate { // Entry
 			FXMLLoader loader = new FXMLLoader(Main.class.getResource("../gui/fxml/MainView.fxml"));
 			anchorPane = (AnchorPane) loader.load();
 			
+			// JFX: ViewController
 			ViewController controller = loader.getController();
 			controller.setMain(this);
 			
+			// Top Menu Pane: (jfx: Application Controls)
+			BorderPane ctlMenu = new BorderPane();
+			MenuItems tS = new MenuItems(); 
+			this.menuBar = tS.dist_menubar(); 
+			ctlMenu.setTop(this.menuBar); 			
+			
+			// Target: scene 
 			Scene scene = new Scene(anchorPane);
 			primaryStage.setScene(scene);
 			primaryStage.show();
